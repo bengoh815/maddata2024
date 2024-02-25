520e5922f47854f22b1e3d91ceee2f5376938889
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-02-24 21:41:19 -0600

    Functioning global dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
+import { useDarkMode } from "./context/darkModeContext";
 
 function App() {
+  const { darkModeEnabled } = useDarkMode();
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <div className={darkModeEnabled ? "dark" : ""}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
